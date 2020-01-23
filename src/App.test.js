ebca0867ec3cb8ae6044459ca1e50d71c981396a
u7chan <34462401+u7chan@users.noreply.github.com>
2020-01-23 15:03:06 +0900

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
+import React from "react";
 import Enzyme from "enzyme";
+import { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+
+import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("<>", () => {
+describe("<App>", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
   it("Test", () => {
-    expect(1).toEqual(1);
+    expect(wrapper.find("h1").text()).toEqual("Hello React");
   });
 });
